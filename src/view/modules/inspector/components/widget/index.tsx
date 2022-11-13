f4f0ef6f648f6a69f154d08f0cd8e1d12b01d6fb
Михаил Реммеле <mihailremmele@MacBook-Pro-Mihail-2.local>
2022-11-13 11:26:56 +0300

    add schemas dependency support

--- src/view/modules/inspector/components/widget/index.tsx
+++ src/view/modules/inspector/components/widget/index.tsx
@@ -1,30 +1,23 @@
 import React, { FC } from 'react'
-import { useTranslation } from 'react-i18next'
 
-import { Field } from '../field'
 import type { WidgetSchema } from '../../../../../types/widget-schema'
 
-import { fieldTypes } from './field-types'
+import { WidgetField } from './widget-field'
 
 interface WidgetProps {
   schema: WidgetSchema
   path: Array<string>
 }
 
-export const Widget: FC<WidgetProps> = ({ schema, path }) => {
-  const { t } = useTranslation()
-
-  return (
-    <div>
-      {schema.fields.map((field) => (
-        <Field
-          key={field.name}
-          path={path.concat(field.name.split('.'))}
-          label={t(`widgetsSchema.${field.title}`)}
-          component={fieldTypes[field.type] ? fieldTypes[field.type] : fieldTypes.string}
-          {...field.type === 'select' ? { referenceId: field.referenceId, references: schema.references } : {}}
-        />
-      ))}
-    </div>
-  )
-}
+export const Widget: FC<WidgetProps> = ({ schema, path }) => (
+  <div>
+    {schema.fields.map((field) => (
+      <WidgetField
+        key={field.name}
+        field={field}
+        path={path}
+        references={schema.references}
+      />
+    ))}
+  </div>
+)
---
+++ src/view/modules/inspector/components/widget/widget-field.tsx
@@ -0,0 +1,40 @@
+import React, { useContext, FC } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { EngineContext } from '../../../../providers'
+import { Field } from '../field'
+import { get, Data } from '../../../../utils/get'
+import type { Field as FieldSchema, Reference } from '../../../../../types/widget-schema'
+
+import { fieldTypes } from './field-types'
+
+interface WidgetFieldProps {
+  field: FieldSchema
+  path: Array<string>
+  references?: Record<string, Reference | undefined>
+}
+
+export const WidgetField: FC<WidgetFieldProps> = ({ field, path, references }) => {
+  const { t } = useTranslation()
+
+  const { sceneContext } = useContext(EngineContext)
+
+  const projectConfig = sceneContext.data.projectConfig as Data
+
+  if (field.dependency) {
+    const value = get(projectConfig, path.concat(field.dependency.name.split('.')))
+
+    if (value !== field.dependency.value) {
+      return null
+    }
+  }
+
+  return (
+    <Field
+      path={path.concat(field.name.split('.'))}
+      label={t(`widgetsSchema.${field.title}`)}
+      component={fieldTypes[field.type] ? fieldTypes[field.type] : fieldTypes.string}
+      {...field.type === 'select' ? { referenceId: field.referenceId, references } : {}}
+    />
+  )
+}
