2f34ada04711534ca96699177349cacb2a7cec38
Mikhail Remmele <mikhail.remmele@gmail.com>
2023-02-12 13:34:08 +0300

    add the possibility to copy id of selected entity in inspector to clipboard

--- src/view/modules/inspector/tabs/entity-inspector/index.tsx
+++ src/view/modules/inspector/tabs/entity-inspector/index.tsx
@@ -1,11 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useCallback, useContext } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Button, Typography } from 'antd'
+import { CopyOutlined } from '@ant-design/icons'
 
 import { SelectedEntityContext } from '../../../../providers'
-
+import { useConfig } from '../../../../hooks'
 import { forms } from '../../forms'
 
+import './style.less'
+
 export const EntityInspector = (): JSX.Element | null => {
+  const { t } = useTranslation()
   const { type, path = [] } = useContext(SelectedEntityContext)
+
+  const entity = useConfig(path) as { id: string }
+  const handleCopyId = useCallback(() => {
+    void navigator.clipboard.writeText(entity.id)
+  }, [entity])
 
   const FormComponent = type ? forms[type] : null
 
@@ -13,5 +24,20 @@ export const EntityInspector = (): JSX.Element | null => {
     return null
   }
 
-  return <FormComponent path={path} />
+  return (
+    <>
+      <header className="entity-inspector__header">
+        <Typography.Text strong>
+          {t(`inspector.entityInspector.${type as string}.title`)}
+        </Typography.Text>
+        <Button
+          icon={<CopyOutlined />}
+          size="small"
+          onClick={handleCopyId}
+          title={t('inspector.entityInspector.copyIdButton.title')}
+        />
+      </header>
+      <FormComponent path={path} />
+    </>
+  )
 }
