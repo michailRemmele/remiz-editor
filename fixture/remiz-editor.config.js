63b44f8e922e2c0fbd7320e7c3260778497e0f69
Mikhail Remmele <mikhail.remmele@gmail.com>
2023-02-12 23:08:03 +0600

    remove test data from fixture editor config

--- fixture/remiz-editor.config.js
+++ fixture/remiz-editor.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  // projectConfig: 'fixture/config.json',
-  // assets: 'fixture',
-  projectConfig: '/Users/mihailremmele/Documents/Projects/ludum-dare-47/public/resources/config.json',
-  assets: '/Users/mihailremmele/Documents/Projects/ludum-dare-47/public',
-  extension: '/Users/mihailremmele/Documents/Projects/ludum-dare-47/build-editor/index.js',
+  projectConfig: 'fixture/config.json',
+  assets: 'fixture',
 }
