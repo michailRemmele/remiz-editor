f3d04f1701b857b5ebad2e1dae7b47a960405c0a
Mikhail Remmele <mikhail.remmele@gmail.com>
2023-02-14 23:07:57 +0600

    add the possibility to delete system or component

--- src/view/modules/inspector/components/entity-list/entity-panel.tsx
+++ src/view/modules/inspector/components/entity-list/entity-panel.tsx
@@ -1,6 +1,9 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useContext, useMemo } from 'react'
 
 import { CollapsePanel } from '../collapse-panel'
+import { SelectedEntityContext } from '../../../../providers'
+import { useCommander } from '../../../../hooks'
+import { deleteValue } from '../../../../commands'
 
 import { EntityForm } from './entity-form'
 import type { Entity, EntityType } from './types'
@@ -14,10 +17,14 @@ export const EntityPanel = <T extends Entity>({
   entity,
   type,
 }: EntityPanelProps<T>): JSX.Element => {
+  const { dispatch } = useCommander()
+  const { path = [] } = useContext(SelectedEntityContext)
+
+  const entityPath = useMemo(() => path.concat(type, `name:${entity.data.name}`), [entity])
+
   const handleDelete = useCallback(() => {
-    // TODO: Implement entity deletion
-    console.log(`Delete entity ${entity.data.name}`)
-  }, [])
+    dispatch(deleteValue(entityPath))
+  }, [dispatch, entityPath])
 
   return (
     <CollapsePanel
