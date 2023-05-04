e60de0d33a12b6cff74295bc1c82e975b0a2e332
Mikhail Remmele <mikhail.remmele@gmail.com>
2023-05-04 21:55:04 +0300

    remove unused sorting layer

--- src/engine/config.ts
+++ src/engine/config.ts
@@ -89,7 +89,6 @@ export const editorConfig: Config = {
               'lights',
               'effectsBack',
               'units',
-              'editor',
             ],
           },
         },
