be258c9f0451f6e660a2209258db01b6dbf077c3
Mikhail Remmele <mikhail.remmele@gmail.com>
2023-06-03 11:35:59 +0300

    remove duplicate code and use getTool utils instead

--- src/engine/systems/pointer-tool-system/consts.ts
+++ src/engine/systems/pointer-tool-system/consts.ts
@@ -1,6 +1,4 @@
 export const TRANSFORM_COMPONENT_NAME = 'transform'
 export const RENDERABLE_COMPONENT_NAME = 'renderable'
 
-export const TOOL_COMPONENT_NAME = 'tool'
-
 export const LEVEL_PATH_LEGTH = 2
--- src/engine/systems/template-tool-system/consts.ts
+++ src/engine/systems/template-tool-system/consts.ts
@@ -1,4 +1,3 @@
-export const TOOL_COMPONENT_NAME = 'tool'
 export const TOOL_NAME = 'template'
 
 export const TRANSFORM_COMPONENT_NAME = 'transform'
