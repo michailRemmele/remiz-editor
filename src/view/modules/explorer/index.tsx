14745d250f88abade9fe3b8b273764f443708ef0
Mikhail Remmele <mikhail.remmele@gmail.com>
2024-08-10 11:58:38 +0300

    change tab for inspector and explorer automatically on entity select

--- src/view/modules/explorer/index.tsx
+++ src/view/modules/explorer/index.tsx
@@ -1,7 +1,13 @@
-import { useCallback, useMemo } from 'react'
+import {
+  useCallback,
+  useEffect,
+  useContext,
+  useState,
+} from 'react'
 import { useTranslation } from 'react-i18next'
 import { Tabs } from 'antd'
 
+import { SelectedEntityContext } from '../../providers'
 import { persistentStorage } from '../../../persistent-storage'
 import { TabsCSS } from '../../common-styles/tabs.style'
 
@@ -10,26 +16,34 @@ import { ExplorerStyled } from './explorer.style'
 
 export const Explorer = (): JSX.Element => {
   const { t } = useTranslation()
+  const { type } = useContext(SelectedEntityContext)
 
-  const initialActiveTab = useMemo(() => persistentStorage.get('explorer.activeTab', 'levels'), [])
+  const [activeTab, setActiveTab] = useState(() => persistentStorage.get('explorer.activeTab', 'levels'))
 
   const handleChange = useCallback((activeKey: string) => {
+    setActiveTab(activeKey)
     persistentStorage.set('explorer.activeTab', activeKey)
   }, [])
 
+  useEffect(() => {
+    if (type) {
+      handleChange(type === 'actor' ? 'level' : type)
+    }
+  }, [type])
+
   return (
     <ExplorerStyled>
-      <Tabs css={TabsCSS} type="card" defaultActiveKey={initialActiveTab} onChange={handleChange}>
-        <Tabs.TabPane tab={t('explorer.tab.levels')} key="levels">
+      <Tabs css={TabsCSS} type="card" activeKey={activeTab} onChange={handleChange}>
+        <Tabs.TabPane tab={t('explorer.tab.levels')} key="level">
           <LevelsTree />
         </Tabs.TabPane>
-        <Tabs.TabPane tab={t('explorer.tab.templates')} key="templates">
+        <Tabs.TabPane tab={t('explorer.tab.templates')} key="template">
           <TemplatesTree />
         </Tabs.TabPane>
-        <Tabs.TabPane tab={t('explorer.tab.scenes')} key="scenes">
+        <Tabs.TabPane tab={t('explorer.tab.scenes')} key="scene">
           <ScenesList />
         </Tabs.TabPane>
-        <Tabs.TabPane tab={t('explorer.tab.loaders')} key="loaders">
+        <Tabs.TabPane tab={t('explorer.tab.loaders')} key="loader">
           <ScenesList isLoaders />
         </Tabs.TabPane>
       </Tabs>
